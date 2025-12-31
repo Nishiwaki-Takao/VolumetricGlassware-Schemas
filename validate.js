e09a188b44e450e9f017bbd0384572c6d722de85
西脇高雄 <garrusarcanjel@gmail.com>
2025-12-31 21:28:22 +0900

    Ajv2020に変更

--- validate.js
+++ validate.js
@@ -1,8 +1,8 @@
 import fs from "fs";
-import Ajv from "ajv";
+import Ajv from "ajv/dist/2020.js";
 
 const map = JSON.parse(fs.readFileSync("validation-map.json", "utf8"));
-const ajv = new Ajv({ allErrors: true, strict: false });
+const ajv = new Ajv2020({ allErrors: true, strict: false });
 
 function validatePair(schemaPath, dataPath) {
   const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"));
